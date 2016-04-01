6e5c689b52159f0a4c23d2144d1deda2bfee597c
Michael de Groot <specamps@gmail.com>
2016-04-02 01:54:55 +0200

    version 0.9

--- RobinNoSpam.user.js
+++ RobinNoSpam.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RobinNoSpam
 // @namespace    http://tampermonkey.net/
-// @version      0.8
+// @version      0.9
 // @description  Fuck the robin vote spam that some india developers made
 // @author       GiveMeAllYourCats
 // @match        *.reddit.com/robin*
