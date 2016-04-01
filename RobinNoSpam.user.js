1eaa0686842e0ea959c63ff9ae4df7c8b9cbab91
Michael de Groot <specamps@gmail.com>
2016-04-02 01:14:29 +0200

    should work now

--- RobinNoSpam.user.js
+++ RobinNoSpam.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RobinNoSpam
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.4
 // @description  Fuck the robin vote spam that some india developers made
 // @author       GiveMeAllYourCats
 // @match        *.reddit.com/robin*
@@ -10,7 +10,12 @@
 
 (function() {
     'use strict';
-    setInterval(function() { $('.robin-message:contains("¦̵̱ ̵̱ ̵̱ ̵̱ ̵̱(̢ ̡͇̅└͇̅┘͇̅ (▤8כ−◦")').remove()}, 10);
-    setInterval(function() { $('.robin-message:contains("Robin Autovoter")').remove()}, 10);
-    setInterval(function() { $('.robin-message:contains("Robin-Grow")').remove()}, 10);
+    
+    var filter = ["¦̵̱ ̵̱ ̵̱ ̵̱ ̵̱(̢ ̡͇̅└͇̅┘͇̅ (▤8כ−◦","Robin Autovoter","Robin-Grow","Confuzet Auto stay voter 1.0","I automatically voted to grow, and so can you!"]
+    
+    setInterval(function() { 
+      for(var i=0;i<filter.length;i++){
+        $('.robin-message:contains("'+filter[i]+'")').remove();
+      }
+    }, 10);
 })();
