1f0105e92a72684189bcce5f4d00b719d0f963c9
Michael de Groot <specamps@gmail.com>
2016-04-02 01:05:00 +0200

    version 0.2: Added "Robin-Grow" spam filter;Using jquery .remove instead of css hide

--- RobinNoSpam.user.js
+++ RobinNoSpam.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RobinNoSpam
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Fuck the robin vote spam that some india developers made
 // @author       GiveMeAllYourCats
 // @match        *.reddit.com/robin*
@@ -10,6 +10,7 @@
 
 (function() {
     'use strict';
-    setInterval(function() { $('.robin-message:contains("¦̵̱ ̵̱ ̵̱ ̵̱ ̵̱(̢ ̡͇̅└͇̅┘͇̅ (▤8כ−◦")').css('display', 'none')}, 10);
-    setInterval(function() { $('.robin-message:contains("Robin Autovoter")').css('display', 'none')}, 10);
+    setInterval(function() { $('.robin-message:contains("¦̵̱ ̵̱ ̵̱ ̵̱ ̵̱(̢ ̡͇̅└͇̅┘͇̅ (▤8כ−◦")').remove()}, 10);
+    setInterval(function() { $('.robin-message:contains("Robin Autovoter")').remove()}, 10);
+    setInterval(function() { $('.robin-message:contains("Robin-Grow")').remove()}, 10);
 })();
