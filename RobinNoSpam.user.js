b7e915765c5e356fc37bcffea28ea340f82cf353
Michael de Groot <specamps@gmail.com>
2016-04-02 02:00:54 +0200

    version 1.0

--- RobinNoSpam.user.js
+++ RobinNoSpam.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RobinNoSpam
 // @namespace    http://tampermonkey.net/
-// @version      0.9
+// @version      1.0
 // @description  Fuck the robin vote spam that some india developers made
 // @author       GiveMeAllYourCats
 // @match        *.reddit.com/robin*
@@ -11,7 +11,7 @@
 (function() {
     'use strict';
     
-    var filter = ["¦̵̱ ̵̱ ̵̱ ̵̱ ̵̱(̢ ̡͇̅└͇̅┘͇̅ (▤8כ−◦","Robin Autovoter","Robin-Grow","Confuzet Auto stay voter 1.0","I automatically voted to grow, and so can you!","Vote Stay!","voted to","卐"];
+    var filter = ["¦̵̱ ̵̱ ̵̱ ̵̱ ̵̱(̢ ̡͇̅└͇̅┘͇̅ (▤8כ−◦","Robin Autovoter","Robin-Grow","Confuzet Auto stay voter 1.0","I automatically voted to grow, and so can you!","Vote Stay!","voted to","卐","Voting will end in approximately"];
     
     $(document).bind('DOMNodeInserted', function(e) {
       for(var i=0;i<filter.length;i++){
